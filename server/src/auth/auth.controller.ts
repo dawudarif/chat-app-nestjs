a6252ba23379cd570a8a54960d785c628f1334d2
Dawood Arif <dawuddarif@gmail.com>
2024-07-17 23:17:35 +0500

    feat: implemented auth guard

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { SigninDTO } from './dto/signIn-dto';
 import { CreateUserDTO } from '../user/dto/create-user-dto';
 import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
+import { Request } from 'express';
+import { CookieStrategy } from './auth.strategy';
+import { CookieGuard } from './cookie.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -24,8 +27,10 @@ export class AuthController {
   }
 
   @Get('profile')
-  async getProfile(@Param('id') id: string) {
-    const getProfile = await this.userService.getUser(id);
-    return getProfile;
+  @UseGuards(CookieGuard)
+  async getProfile(@Req() req: Request) {
+    console.log(req.user);
+
+    return req.user;
   }
 }
---
+++ server/src/auth/auth.strategy.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class CookieStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          let token = null;
+          if (req && req.cookies) {
+            token = req.cookies['jwt'];
+          }
+
+          return token;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('secret'),
+    });
+  }
+
+  async validate(payload: any) {
+    return {
+      userId: payload.sub,
+      email: payload.email,
+    };
+  }
+}
