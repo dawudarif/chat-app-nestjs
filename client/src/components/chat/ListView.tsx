033d73db93c3a13d901affb516790beb766732af
Dawood Arif <dawuddarif@gmail.com>
2024-09-13 11:11:25 +0500

    feat: search conversations

--- client/src/components/chat/ListView.tsx
+++ client/src/components/chat/ListView.tsx
@@ -1,9 +1,10 @@
 import clsx from "clsx";
 import Link from "next/link";
-import React, { useState } from "react";
-import { ConversationData } from "../../types/types";
+import React, { useEffect, useState } from "react";
+import { ConversationData, SearchData } from "../../types/types";
 import Input from "../Custom/Input";
 import { useUserContext } from "../../context/UserContext";
+import api from "../../utils/api";
 
 interface ListViewProps {
   conversationData?: ConversationData[];
@@ -17,15 +18,40 @@ const ListView: React.FC<ListViewProps> = ({
   loading,
 }) => {
   const [search, setSearch] = useState("");
+  const [searchData, setSearchData] = useState<SearchData[]>([]);
+  const [searchLoading, setSearchLoading] = useState(false);
   const { userInfo } = useUserContext();
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
+  const searchConversations = async () => {
+    try {
+      const response = await api.post("/conversation/search", { text: search });
+
+      if (response.status === 201) {
+        setSearchData(response.data);
+      }
+    } catch (error) {
+    } finally {
+      setSearchLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (search === "") {
+      setSearchData([]);
+      return;
+    }
+    setSearchLoading(true);
+    const timeout = setTimeout(() => searchConversations(), 1000);
+
+    return () => clearTimeout(timeout);
+  }, [search]);
+
   return (
     <div className="flex flex-col justify-start items-center w-1/4 p-2 border-r border-brand-black h-[100vh]">
-      {loading}
       <Input
         handleChange={handleInputChange}
         value={search}
@@ -36,7 +62,7 @@ const ListView: React.FC<ListViewProps> = ({
         type="text"
       />
 
-      {loading
+      {loading || searchLoading
         ? Array(10)
             .fill(null)
             .map((_, i) => {
@@ -52,6 +78,28 @@ const ListView: React.FC<ListViewProps> = ({
                 </div>
               );
             })
+        : search !== ""
+        ? searchData &&
+          searchData?.length > 0 &&
+          searchData.map((item: SearchData) => {
+            return (
+              <Link
+                href={`/${item.id}`}
+                key={item.id}
+                className="w-full border-b border-brand-dark-gray p-1 group"
+              >
+                <div className="group-hover:bg-brand-filled-blue/90 group-hover:text-white text-brand-black group-hover:rounded-lg transition-all duration-100 p-2 my-[.15rem] cursor-pointer">
+                  <h1 className="font-medium text-base capitalize">
+                    {item.participants[0].user.name}
+                  </h1>
+
+                  <h6 className="text-sm font-normal">
+                    @{item.participants[0].user.username}
+                  </h6>
+                </div>
+              </Link>
+            );
+          })
         : conversationData &&
           conversationData?.length > 0 &&
           conversationData.map((item: ConversationData) => {
