508e14f5cd49118ae00fdb60a24a0b85674b99c5
Dawood Arif <dawuddarif@gmail.com>
2024-09-21 20:14:27 +0500

    feat: list view
    conversation message updates, refactored web sockets

--- client/src/redux/features/conversationSlice.ts
+++ client/src/redux/features/conversationSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { ConversationData } from "../../types/types";
 
 const conversationSlice = createSlice({
@@ -14,9 +14,26 @@ const conversationSlice = createSlice({
     setConversations: (state, action) => {
       state.conversations = action.payload;
     },
+    updateConversations: (state, action: PayloadAction<ConversationData>) => {
+      let initialConversations = [...state.conversations];
+
+      const checkExisting = initialConversations.find(
+        (c) => c.id === action.payload.id
+      );
+      if (checkExisting) {
+        state.conversations = initialConversations.map((c) => {
+          if (c.id === action.payload.id) {
+            return action.payload;
+          }
+          return c;
+        });
+      } else {
+        state.conversations = [...initialConversations, action.payload];
+      }
+    },
   },
 });
 
-export const { setCurrentConversation, setConversations } =
+export const { setCurrentConversation, setConversations, updateConversations } =
   conversationSlice.actions;
 export default conversationSlice.reducer;
